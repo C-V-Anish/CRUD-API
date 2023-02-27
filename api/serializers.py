fb8b79c8f72e7fa7c543641ad99d2d7a7bfd8639
C V Anish <mahadevrao77@gmail.com>
2023-02-27 23:01:25 +0530

    Validators

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Student
 
+#Validators
+def start_with_capital(value):
+    if value[0].upper()!=value[0]:
+        raise serializers.ValidationError("First Letter of Name must be a capital letter")
+
 class StudentSerializer(serializers.Serializer):
-    name=serializers.CharField(max_length=50)
+    name=serializers.CharField(max_length=50,validators=[start_with_capital])
     roll=serializers.IntegerField()
     city=serializers.CharField(max_length=20)
 
@@ -15,3 +20,15 @@ class StudentSerializer(serializers.Serializer):
         instance.city=validated_data.get('city',instance.city)
         instance.save()
         return instance
+    
+    # Field Level Validatiom
+    # def validate_roll(self, value):
+    #     if(value>100):
+    #         raise serializers.ValidationError("Seat Full")
+    #     return value
+
+    def validate(self, data):
+        city=data.get('city')
+        if city.lower()=='bhubaneswar':
+            raise serializers.ValidationError("College must be IIIT Bhubaneswar")
+        return data
--- test.py
+++ test.py
@@ -3,16 +3,16 @@ import json
 
 URL="http://127.0.0.1:8000/studentapi/"
 
-def get_data(id=None):
-    data={}
-    if id is not None:
-        data={'id':id}
-    json_data=json.dumps(data)
-    r=requests.get(url=URL,data=json_data)
-    data=r.json()
-    print(data)
+# def get_data(id=None):
+#     data={}
+#     if id is not None:
+#         data={'id':id}
+#     json_data=json.dumps(data)
+#     r=requests.get(url=URL,data=json_data)
+#     data=r.json()
+#     print(data)
 
-get_data()
+# get_data()
 
 def post_data():
     name=input("Enter Student Name : ")
@@ -31,28 +31,28 @@ def post_data():
 
 post_data()
 
-def update_data():
-    data={
-        'id':4,
-        'name':'Prabhu',
-        'roll':6,
-        'city':'Bangalore'
-    }
-    json_data=json.dumps(data)
-    r=requests.put(url=URL,data=json_data)
-    data=r.json()
-    print(data)
+# def update_data():
+#     data={
+#         'id':4,
+#         'name':'Prabhu',
+#         'roll':6,
+#         'city':'Bangalore'
+#     }
+#     json_data=json.dumps(data)
+#     r=requests.put(url=URL,data=json_data)
+#     data=r.json()
+#     print(data)
 
-update_data()
+# update_data()
 
-def delete_data(id):
-    data={'id':id}
-    json_data=json.dumps(data)
-    r=requests.delete(url=URL,data=json_data)
-    data=r.json()
-    print(data)
+# def delete_data(id):
+#     data={'id':id}
+#     json_data=json.dumps(data)
+#     r=requests.delete(url=URL,data=json_data)
+#     data=r.json()
+#     print(data)
 
-delete_data(2)
+# delete_data(2)
 
 
 
